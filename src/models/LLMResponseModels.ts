6e177be264724de5c6985e3037f6577f81c6f241
Apprivoisé <221250137@smail.nju.edu.cn>
2025-05-25 19:34:20 +0800

    feat: 增加与后端对应的 3 + 1 接口

--- src/models/LLMResponseModels.ts
+++ src/models/LLMResponseModels.ts
@@ -11,3 +11,10 @@ export interface LLMResponse {
     answer: string;
     source_documents: source_document[];
 }
+
+export interface PatientInfo {
+    patient_name: string;
+    patient_age: string;
+    patient_gender: string;
+    patient_medical_history: string;
+}
